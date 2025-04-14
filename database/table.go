5018ac92d6555b32ded6212e0edcf80a1d3d38da
Toft <toft.dah@gmail.com>
2025-04-14 11:43:49 +0300

    fix initial post

--- database/table.go
+++ database/table.go
@@ -144,16 +144,10 @@ func MakeTables(db *sql.DB) {
 	//Insert initial data into Post
 	insertPostQuery := `
 	INSERT INTO post (title, content, user_id, created_at)
-	SELECT 'THIS IS THE THIRD POST', 'This is the third post!', 1, datetime('now')
-	WHERE NOT EXISTS (SELECT 1 FROM post WHERE title = 'THIS IS THE THIRD POST');
-
-	INSERT INTO post (title, content, user_id, created_at)
-	SELECT 'THIS IS THE SECOND POST', 'This is the second post!', 1, datetime('now')
-	WHERE NOT EXISTS (SELECT 1 FROM post WHERE title = 'THIS IS THE SECOND POST');
-
-	INSERT INTO post (title, content, user_id, created_at)
 	SELECT 'Welcome to the forum', 'This is the first post!', 1, datetime('now')
-	WHERE NOT EXISTS (SELECT 1 FROM post WHERE title = 'Welcome to the forum');
+	WHERE NOT EXISTS (
+	SELECT 1 FROM post WHERE title = 'Welcome to the forum'
+	);
 `
 	result, err := db.Exec(insertPostQuery)
 	if err != nil {
@@ -170,11 +164,7 @@ func MakeTables(db *sql.DB) {
 
 	insertPostCategoryQuery := `
     INSERT INTO Post_category (post_id, category_id)
-	SELECT id, 1 FROM Post WHERE title = 'Welcome to the forum'
-    UNION ALL
-    SELECT id, 2 FROM Post WHERE title = 'THIS IS THE SECOND POST'
-    UNION ALL
-    SELECT id, 3 FROM Post WHERE title = 'THIS IS THE THIRD POST';
+	VALUES (?, 1);
 `
 
 	if _, err := db.Exec(insertPostCategoryQuery, int(lastInsertID)); err != nil {
