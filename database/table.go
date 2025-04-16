d75656c66c52235286558fd45c7ede737f0bbe6b
Toft <toft.dah@gmail.com>
2025-04-16 17:05:51 +0300

    still looking

--- database/table.go
+++ database/table.go
@@ -9,18 +9,18 @@ import (
 func MakeTables(db *sql.DB) {
 
 	createUserTableQuery := `
-CREATE TABLE IF NOT EXISTS User (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	email TEXT UNIQUE NOT NULL,
-	username TEXT UNIQUE NOT NULL COLLATE NOCASE,
-	password TEXT NOT NULL,
-	firstname TEXT NOT NULL,
-	lastname TEXT NOT NULL,
-	age INTEGER NOT NULL,
-	gender TEXT NOT NULL,
-	created_at TEXT NOT NULL,
-	status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'deleted'))
-);`
+		CREATE TABLE IF NOT EXISTS User (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT UNIQUE NOT NULL,
+		username TEXT UNIQUE NOT NULL COLLATE NOCASE,
+		password TEXT NOT NULL,
+		firstname TEXT NOT NULL,
+		lastname TEXT NOT NULL,
+		age INTEGER NOT NULL,
+		gender TEXT NOT NULL,
+		created_at TEXT NOT NULL,
+		status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'deleted'))
+	);`
 	if _, err := db.Exec(createUserTableQuery); err != nil {
 		fmt.Println("Error creating User table:", err)
 		return
@@ -34,54 +34,57 @@ CREATE TABLE IF NOT EXISTS User (
    		created_at TEXT NOT NULL,
     	FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
 	);`
+	//status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'deleted'))
 	if _, err := db.Exec(createPostTableQuery); err != nil {
 		fmt.Println("Error creating Post table:", err)
+		return
+	}
+	createCommentTableQuery := `
+		CREATE TABLE IF NOT EXISTS Comment (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		content TEXT NOT NULL,
+		post_id INTEGER NOT NULL,
+		user_id INTEGER NOT NULL,
+		created_at TEXT NOT NULL,
+		FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE,
+		FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
+	);`
+	//status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'deleted'))
+	if _, err := db.Exec(createCommentTableQuery); err != nil {
+		fmt.Println("Error creating Comment table:", err)
 		return
 	}
 	createMessageTableQuery := `
 		CREATE TABLE IF NOT EXISTS Message (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		sender TEXT NOT NULL,
-    	receiver TEXT NOT NULL,
-   		user_id INTEGER NOT NULL,
+		receiver TEXT NOT NULL,
+		user_id INTEGER NOT NULL,
 		content TEXT NOT NULL,
-   		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    	FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
 	);`
 	if _, err := db.Exec(createMessageTableQuery); err != nil {
 		fmt.Println("Error creating Message table:", err)
 		return
 	}
-	createCommentTableQuery := `
-		CREATE TABLE IF NOT EXISTS Comment (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	content TEXT NOT NULL,
-    	post_id INTEGER NOT NULL,
-    	user_id INTEGER NOT NULL,
-    	created_at TEXT NOT NULL,
-    	FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE,
-    	FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
-	);`
-	if _, err := db.Exec(createCommentTableQuery); err != nil {
-		fmt.Println("Error creating Comment table:", err)
-		return
-	}
 	createCategoryTableQuery := `
 		CREATE TABLE IF NOT EXISTS Category (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	name TEXT UNIQUE NOT NULL
+		name TEXT UNIQUE NOT NULL
 	);`
+	//status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'inactive', 'deleted'))
 	if _, err := db.Exec(createCategoryTableQuery); err != nil {
 		fmt.Println("Error creating Category table:", err)
 		return
 	}
 	createPost_CategoryTableQuery := `
 		CREATE TABLE IF NOT EXISTS Post_Category (
-    	id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	category_id INTEGER NOT NULL,
-    	post_id INTEGER NOT NULL,
-    	FOREIGN KEY (category_id) REFERENCES Category (id) ON DELETE CASCADE,
-    	FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		category_id INTEGER NOT NULL,
+		post_id INTEGER NOT NULL,
+		FOREIGN KEY (category_id) REFERENCES Category (id) ON DELETE CASCADE,
+		FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE
 	);`
 	if _, err := db.Exec(createPost_CategoryTableQuery); err != nil {
 		fmt.Println("Error creating Post_Category table:", err)
@@ -90,14 +93,15 @@ CREATE TABLE IF NOT EXISTS User (
 	createLikeTableQuery := `
 		CREATE TABLE IF NOT EXISTS Like (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-    	user_id INTEGER NOT NULL,
-    	post_id INTEGER,
-    	comment_id INTEGER,
-   		created_at TEXT NOT NULL,
-    	type INTEGER NOT NULL,
-    	FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE,
-    	FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE,
-    	FOREIGN KEY (comment_id) REFERENCES Comment (id) ON DELETE CASCADE
+		user_id INTEGER NOT NULL,
+		post_id INTEGER,
+		comment_id INTEGER,
+		created_at TEXT NOT NULL,
+		updated_at TEXT,
+		type INTEGER NOT NULL,
+		FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE,
+		FOREIGN KEY (post_id) REFERENCES Post (id) ON DELETE CASCADE,
+		FOREIGN KEY (comment_id) REFERENCES Comment (id) ON DELETE CASCADE
 	);`
 	if _, err := db.Exec(createLikeTableQuery); err != nil {
 		fmt.Println("Error creating Like table:", err)
@@ -107,11 +111,11 @@ CREATE TABLE IF NOT EXISTS User (
 		CREATE TABLE IF NOT EXISTS Session (
 		id TEXT PRIMARY KEY,
 		status TEXT NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'expired', 'inactive', 'deleted')),
-    	user_id INTEGER NOT NULL,
-    	created_at TEXT NOT NULL,
+		user_id INTEGER NOT NULL,
+		created_at TEXT NOT NULL,
 		updated_at TEXT NOT NULL,
 		expired_at TEXT NOT NULL,
-    	FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
+		FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
 	);`
 	if _, err := db.Exec(createSessionTableQuery); err != nil {
 		fmt.Println("Error creating Session table:", err)
